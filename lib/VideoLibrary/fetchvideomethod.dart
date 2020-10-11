5d1260ef463bfa1f695b71c972f432ddb4cab090
Rakshit Khandelwal <rakshitkhandelwal1234@gmail.com>
2020-10-11 12:57:45 +0530

    final changes

--- lib/VideoLibrary/fetchvideomethod.dart
+++ lib/VideoLibrary/fetchvideomethod.dart
@@ -7,9 +7,9 @@ class GetVideo {
     QuerySnapshot querySnapshot = await FirebaseFirestore.instance
         .collection("Videos")
         .orderBy("timestamp")
-        .getDocuments();
-    for (var i = 0; i < querySnapshot.documents.length; i++) {
-      videoList.add(VideoDetail.fromMap(querySnapshot.documents[i].data()));
+        .get();
+    for (var i = 0; i < querySnapshot.docs.length; i++) {
+      videoList.add(VideoDetail.fromMap(querySnapshot.docs[i].data()));
     }
     return videoList;
   }
