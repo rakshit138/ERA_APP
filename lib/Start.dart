b9dbf6487b0b164198ad1ea55faa127f54aa1c74
Rakshit Khandelwal <rakshitkhandelwal1234@gmail.com>
2020-10-11 12:29:20 +0530

    start page changes

--- lib/Start.dart
+++ lib/Start.dart
@@ -19,84 +19,90 @@ class _StartState extends State<Start> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 35.0),
-              Container(
-                height: 400,
-                child: Image(
-                  image: AssetImage("assets/images/logopng.png"),
-                  fit: BoxFit.contain,
-                ),
-              ),
-              SizedBox(height: 20),
-              RichText(
-                  text: TextSpan(
-                      text: 'Welcome to ',
-                      style: TextStyle(
-                          fontSize: 25.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                      children: <TextSpan>[
-                    TextSpan(
-                        text: 'ERA',
-                        style: TextStyle(
-                            fontSize: 30.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.amber))
-                  ])),
-              SizedBox(height: 10.0),
-              SizedBox(height: 30.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+    return Container(
+      child: Container(
+        padding: EdgeInsets.all(10),
+        decoration: new BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/BAckground.png"),
+                fit: BoxFit.cover)),
+        child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
-                  RaisedButton(
-                      padding: EdgeInsets.only(left: 30, right: 30),
-                      onPressed: navigateToLogin,
-                      child: Text(
-                        'LOGIN',
-                        style: TextStyle(
-                          fontFamily: 'Merriweather',
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xff03258C),
+                  Container(
+                    height: 300,
+                    padding: EdgeInsets.fromLTRB(20, 40, 20, 20),
+                    child: Image(
+                      image: AssetImage("assets/images/logo png 3.png"),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  SizedBox(height: 40.0),
+                  Container(
+                    width: 200,
+                    child: RaisedButton(
+                        padding: EdgeInsets.only(left: 30, right: 30),
+                        onPressed: navigateToLogin,
+                        child: Text(
+                          'LOGIN',
+                          style: TextStyle(
+                            fontFamily: 'Merriweather',
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff03258C),
+                          ),
                         ),
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      color: Colors.amber),
-                  SizedBox(width: 20.0),
-                  RaisedButton(
-                      padding: EdgeInsets.only(left: 30, right: 30),
-                      onPressed: navigateToRegister,
-                      child: Text(
-                        'REGISTER',
-                        style: TextStyle(
-                          fontFamily: 'Merriweather',
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xff03258C),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
                         ),
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      color: Colors.amber),
+                        color: Colors.amber),
+                  ),
+                  Container(
+                    width: 200,
+                    child: RaisedButton(
+                        padding: EdgeInsets.only(left: 30, right: 30),
+                        onPressed: navigateToRegister,
+                        child: Text(
+                          'REGISTER',
+                          style: TextStyle(
+                            fontFamily: 'Merriweather',
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff03258C),
+                          ),
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        color: Colors.amber),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    width: 200,
+                    child: SignInButton(
+                      Buttons.Google,
+                      text: "Sign up with Google",
+                      onPressed: () {},
+                    ),
+                  ),
                 ],
               ),
-              SizedBox(height: 20.0),
-              SignInButton(
-                Buttons.Google,
-                text: "Sign up with Google",
-                onPressed: () {},
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  '\u00a9 ERA-Assistant To Redefine Education',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Merriweather',
+                      fontSize: 12,
+                      decoration: TextDecoration.none),
+                ),
               )
-            ],
-          ),
-        ),
+            ]),
       ),
     );
   }
