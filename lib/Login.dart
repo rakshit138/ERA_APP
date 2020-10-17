7719e58d61d5ebe267edf8fc3bc527c623099bfc
Rakshit Khandelwal <rakshitkhandelwal1234@gmail.com>
2020-10-17 14:59:13 +0530

    bg image login

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,5 +1,5 @@
 import 'package:ERA/HomePage.dart';
-import 'package:ERA/loading.dart';
+// import 'package:ERA/loading.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'SignUp.dart';
@@ -76,130 +76,140 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(10),
-        decoration: new BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/images/BAckground.png"),
-                fit: BoxFit.cover)),
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 300,
-              padding: EdgeInsets.fromLTRB(20, 40, 20, 20),
-              child: Image(
-                image: AssetImage("assets/images/logo png 3.png"),
-                fit: BoxFit.fill,
-              ),
-            ),
-            Container(
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.all(15),
-                      child: Container(
-                        child: TextFormField(
-                            validator: (input) {
-                              if (input.isEmpty) return 'Enter Email';
-                            },
-                            decoration: InputDecoration(
-                                contentPadding: const EdgeInsets.all(10),
-                                enabledBorder: UnderlineInputBorder(
-                                  borderSide:
-                                      new BorderSide(color: Color(0xff03258C)),
-                                  borderRadius: new BorderRadius.circular(25.7),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: new BorderSide(
-                                      color: Color(0xff03258C), width: 3),
-                                  borderRadius: new BorderRadius.circular(25.7),
-                                ),
-                                filled: true,
-                                fillColor: Colors.amber,
-                                labelStyle: TextStyle(
-                                    color: Color(0xff03258C),
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18),
-                                labelText: 'Email',
-                                prefixIcon: Icon(
-                                  Icons.email,
-                                  color: Color(0xff03258C),
-                                )),
-                            onSaved: (input) => _email = input),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(15),
-                      child: Container(
-                        child: TextFormField(
-                            validator: (input) {
-                              if (input.length < 6)
-                                return 'Provide Minimum 6 Character';
-                            },
-                            decoration: InputDecoration(
-                                contentPadding: const EdgeInsets.all(10),
-                                enabledBorder: UnderlineInputBorder(
-                                  borderSide:
-                                      new BorderSide(color: Color(0xff03258C)),
-                                  borderRadius: new BorderRadius.circular(25.7),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: new BorderSide(
-                                      color: Color(0xff03258C), width: 3),
-                                  borderRadius: new BorderRadius.circular(25.7),
-                                ),
-                                filled: true,
-                                fillColor: Colors.amber,
-                                labelStyle: TextStyle(
-                                    color: Color(0xff03258C),
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18),
-                                labelText: 'Password',
-                                prefixIcon: Icon(
-                                  Icons.lock,
-                                  color: Color(0xff03258C),
-                                )),
-                            obscureText: true,
-                            onSaved: (input) => _password = input),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    RaisedButton(
-                      padding: EdgeInsets.fromLTRB(70, 10, 70, 10),
-                      onPressed: login,
-                      child: Text('LOGIN',
-                          style: TextStyle(
-                              fontFamily: 'Merriweather',
-                              color: Color(0xff03258C),
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold)),
-                      color: Colors.amber,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: GestureDetector(
-                child: Text(
-                  'Create an Account?',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold),
-                ),
-                onTap: navigateToSignUp,
-              ),
-            )
-          ],
+      body: Stack(children: [
+        Container(
+          decoration: new BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/BAckground.png"),
+                  fit: BoxFit.cover)),
         ),
-      ),
+        SingleChildScrollView(
+          child: Container(
+            color: Colors.transparent,
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 300,
+                  padding: EdgeInsets.fromLTRB(20, 40, 20, 20),
+                  child: Image(
+                    image: AssetImage("assets/images/logo png 3.png"),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Container(
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(15),
+                          child: Container(
+                            child: TextFormField(
+                                validator: (input) {
+                                  if (input.isEmpty) return 'Enter Email';
+                                },
+                                decoration: InputDecoration(
+                                    contentPadding: const EdgeInsets.all(10),
+                                    enabledBorder: UnderlineInputBorder(
+                                      borderSide: new BorderSide(
+                                          color: Color(0xff03258C)),
+                                      borderRadius:
+                                          new BorderRadius.circular(25.7),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: new BorderSide(
+                                          color: Color(0xff03258C), width: 3),
+                                      borderRadius:
+                                          new BorderRadius.circular(25.7),
+                                    ),
+                                    filled: true,
+                                    fillColor: Colors.amber,
+                                    labelStyle: TextStyle(
+                                        color: Color(0xff03258C),
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18),
+                                    labelText: 'Email',
+                                    prefixIcon: Icon(
+                                      Icons.email,
+                                      color: Color(0xff03258C),
+                                    )),
+                                onSaved: (input) => _email = input),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(15),
+                          child: Container(
+                            child: TextFormField(
+                                validator: (input) {
+                                  if (input.length < 6)
+                                    return 'Provide Minimum 6 Character';
+                                },
+                                decoration: InputDecoration(
+                                    contentPadding: const EdgeInsets.all(10),
+                                    enabledBorder: UnderlineInputBorder(
+                                      borderSide: new BorderSide(
+                                          color: Color(0xff03258C)),
+                                      borderRadius:
+                                          new BorderRadius.circular(25.7),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: new BorderSide(
+                                          color: Color(0xff03258C), width: 3),
+                                      borderRadius:
+                                          new BorderRadius.circular(25.7),
+                                    ),
+                                    filled: true,
+                                    fillColor: Colors.amber,
+                                    labelStyle: TextStyle(
+                                        color: Color(0xff03258C),
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18),
+                                    labelText: 'Password',
+                                    prefixIcon: Icon(
+                                      Icons.lock,
+                                      color: Color(0xff03258C),
+                                    )),
+                                obscureText: true,
+                                onSaved: (input) => _password = input),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        RaisedButton(
+                          padding: EdgeInsets.fromLTRB(70, 10, 70, 10),
+                          onPressed: login,
+                          child: Text('LOGIN',
+                              style: TextStyle(
+                                  fontFamily: 'Merriweather',
+                                  color: Color(0xff03258C),
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold)),
+                          color: Colors.amber,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: GestureDetector(
+                    child: Text(
+                      'Create an Account?',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    onTap: navigateToSignUp,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
